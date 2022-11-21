1f4beb24114c16b91aa04a17cc82aeb292046051
Bao <v.baovt2@vinbigdata.com>
2022-11-21 11:37:38 +0700

    Save counter to bundle

--- app/src/main/java/com/baovt/androidweek5/MainActivity.java
+++ app/src/main/java/com/baovt/androidweek5/MainActivity.java
@@ -16,11 +16,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         counterText = findViewById(R.id.counter_text);
+
+        //state restore
+        if (savedInstanceState != null) {
+            count = savedInstanceState.getInt("counter");
+            counterText.setText(String.valueOf(count));
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("counter", count);
     }
 
     public void countUp(View view) {
         count++;
         if (counterText != null)
-            counterText.setText(Integer.toString(count));
+            counterText.setText(String.valueOf(count));
     }
 }
